7ff80c0d9404a540a83a2db9639315eaf1c094c5
Howard Tang <tangh@google.com>
2014-02-26 12:01:50 +0000

    Seperate filereading from fn.

--- cli.js
+++ cli.js
@@ -1,5 +1,6 @@
 // CLI example.
+var fs = require('fs');
 var removeWhitespace = require('./lib/remove-whitespace');
 var file = process.argv[2];
 
-process.stdout.write(removeWhitespace(file));
+process.stdout.write(removeWhitespace(fs.readFileSync(file, 'utf8')));
--- test/test.js
+++ test/test.js
@@ -1,12 +1,13 @@
+var fs = require('fs');
 var test = require('tape');
 var removeWhitespace = require('../lib/remove-whitespace');
 
-test('Should split a read file into list of lines, and remove any that are empty', function(t){
+test('Should split a read file into list of lines, and remove any that are empty', function(t) {
 	t.plan(1);
-	t.equal(removeWhitespace('./test/test.txt'), 'hello\nworld');
+	t.equal(removeWhitespace(fs.readFileSync('./test/test.txt', 'utf8')), 'hello\nworld');
 });
 
 test('Should remove any lines that include only whitespace', function(t) {
 	t.plan(1);
-	t.equal(removeWhitespace('./test/test1.txt'), 'hello\nworld');
+	t.equal(removeWhitespace(fs.readFileSync('./test/test1.txt', 'utf8')), 'hello\nworld');
 });
